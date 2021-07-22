d0d3bd267db90b49a94905e605093e0e5c85542d
Andrew Piper <andrew@threax.com>
2021-07-22 13:42:22 -0400

    Updated to htmlrapier 25.

--- Shopping/Views/Home/Stores.ts
+++ Shopping/Views/Home/Stores.ts
@@ -1,7 +1,7 @@
-import * as standardCrudPage from 'hr.widgets.StandardCrudPage';
-import * as startup from 'clientlibs.startup';
-import * as deepLink from 'hr.deeplink';
-import { StoreCrudInjector } from 'clientlibs.StoreCrudInjector';
+import * as standardCrudPage from 'htmlrapier.widgets/src/StandardCrudPage';
+import * as startup from 'Client/Libs/startup';
+import * as deepLink from 'htmlrapier/src/deeplink';
+import { StoreCrudInjector } from 'Client/Libs/StoreCrudInjector';
 
 var injector = StoreCrudInjector;
 
